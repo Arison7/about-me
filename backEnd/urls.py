f9df4e43ccad3f7ebe27bad9fb2203ba4de04be0
Arison <arison646@gmail.com>
2023-09-06 13:38:43 +0200

    clean up and configurations

--- backEnd/urls.py
+++ backEnd/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for aboutMe project.
+URL configuration for backEnd project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
