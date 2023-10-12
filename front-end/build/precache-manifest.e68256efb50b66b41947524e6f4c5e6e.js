78c74e252eccad3606f1e4f6e4bef94a8fee3ec5
Arison <arison646@gmail.com>
2023-10-12 13:32:12 +0200

    adds build for hosting

---
+++ front-end/build/precache-manifest.e68256efb50b66b41947524e6f4c5e6e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca30cf0a0803c81ffbac",
+    "url": "/static/css/main.96996599.chunk.css"
+  },
+  {
+    "revision": "ca30cf0a0803c81ffbac",
+    "url": "/static/js/main.ca30cf0a.chunk.js"
+  },
+  {
+    "revision": "ec5ddaa9f6f0a593122a",
+    "url": "/static/js/1.ec5ddaa9.chunk.js"
+  },
+  {
+    "revision": "f87b8641b37a07839281",
+    "url": "/static/js/2.f87b8641.chunk.js"
+  },
+  {
+    "revision": "e01dcd01bb42adeaa836",
+    "url": "/static/js/runtime~main.e01dcd01.js"
+  },
+  {
+    "revision": "55c77b0ef82f3db67d5fed492885a699",
+    "url": "/static/media/wood.55c77b0e.jpg"
+  },
+  {
+    "revision": "308725d85b204a33f47a81cf3cab9c61",
+    "url": "/static/media/Flash.308725d8.ttf"
+  },
+  {
+    "revision": "606ace364563083c975510f64fd646dd",
+    "url": "/index.html"
+  }
+];
